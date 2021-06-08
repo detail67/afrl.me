b7852fa6c8eb51f8b4929aace6783f40ca4368b5
detail67 <85509635+detail67@users.noreply.github.com>
2021-06-08 14:55:25 +0200

    Update back_to_top.js

--- js/back_to_top.js
+++ js/back_to_top.js
@@ -1,20 +1,16 @@
 $(document).ready(function(){
      $(window).scroll(function () {
             if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
+                $('#upupup').fadeIn();
             } else {
-                $('#back-to-top').fadeOut();
+                $('#upupup').fadeOut();
             }
         });
         // scroll body to 0px on click
-        $('#back-to-top').click(function () {
-            //$('#back-to-top').tooltip('hide');
+        $('#upupup').click(function () {
             $('body,html').animate({
                 scrollTop: 0
             }, 800);
             return false;
         });
-
-        //$('#back-to-top').tooltip('show');
-
 });
